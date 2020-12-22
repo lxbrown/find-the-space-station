2c4d15222a6486642d16572d6d463b5d2732c086
Alex Brown <lxbrown13@gmail.com>
2020-12-22 15:32:08 -0500

    Add page title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import { useSnackbar } from 'react-simple-snackbar';
 
 import ScatterplotOverlay from './overlay/scatterplot-overlay';
+import Title from './title';
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -32,9 +33,16 @@ function App() {
       //   positionFeed.push([-122 + positionFeed.length, 37]);
       // }
       // setPositions(positionFeed);
+      // if (positionFeed.length === 1) {
+      //   setViewport({
+      //     latitude: positionFeed[0][1],
+      //     longitude: positionFeed[0][0],
+      //     zoom: 2
+      //   });
+      // }
       
       closeSnackbar();
-      axios.get('https://api.wheretheiss.at/v1/satellites/25542')
+      axios.get('https://api.wheretheiss.at/v1/satellites/25544')
       .then(response => {
         failCount = 0;
         setPositions([]);
@@ -45,7 +53,7 @@ function App() {
             latitude: positionFeed[0][1],
             longitude: positionFeed[0][0],
             zoom: 2
-          })
+          });
         }
       })
       .catch(() => {
@@ -69,22 +77,25 @@ function App() {
   }, [stopUpdates]);
 
   return (
-    <MapGL
-      {...viewport}
-      width="100vw"
-      height="100vh"
-      mapStyle="mapbox://styles/mapbox/dark-v9"
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-      mapboxApiAccessToken={env.MAPBOX_ACCESS_TOKEN}>
-      <ScatterplotOverlay
-        locations={positions}
-        dotRadius={10}
-        globalOpacity={0.8}
-        compositeOperation="lighter"
-        dotFill="#00a8fe"
-        renderWhileDragging={true}
-      />
-    </MapGL>
+    <>
+      <MapGL
+        {...viewport}
+        width="100vw"
+        height="100vh"
+        mapStyle="mapbox://styles/mapbox/dark-v9"
+        onViewportChange={nextViewport => setViewport(nextViewport)}
+        mapboxApiAccessToken={env.MAPBOX_ACCESS_TOKEN}>
+        <ScatterplotOverlay
+          locations={positions}
+          dotRadius={10}
+          globalOpacity={0.8}
+          compositeOperation="lighter"
+          dotFill="#00a8fe"
+          renderWhileDragging={true}
+        />
+      </MapGL>
+      <Title/>
+    </>
   );
 }
 
---
+++ src/title.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+import './title.css';
+
+export default function Title() {
+    return (
+        <div className="title-panel">
+            <h2 className="title">Find the space station</h2>
+            <p className="description">254 miles up</p>
+        </div>
+    );
+}
